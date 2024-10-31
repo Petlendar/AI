9a6918cbb985a36912bd8040e9fcac5b043b00b9
LeeKyungBum <lkb3311@gmail.com>
2024-10-31 21:48:53 +0900

    #19 (feat): imageUrl 값이 None인 경우 예외 처리

--- GetData.py
+++ GetData.py
@@ -1,9 +1,9 @@
 import requests
 
 # 반려동물 정보 조회 URL
-def pet_info(pet_id, jwt_token):
+def pet_info(petId, jwt_token):
     # 실제 petId 값으로 교체 필요 (프론트에서 받아야함)
-    url = f"http://114.70.216.57/pet/api/pet/{pet_id}"
+    url = f"http://114.70.216.57/pet/api/pet/{petId}"
 
 
     # 실제 JWT 토큰으로 대체 필요 (프론트에서 받아야함)
@@ -22,12 +22,27 @@ def pet_info(pet_id, jwt_token):
         pet_birth = response_data.get("body", {}).get("birth") # 반려 동물 생일
         pet_category = response_data.get("body", {}).get("category") # 반려동물의 종
         pet_weight = response_data.get("body", {}).get("weight") # 몸무게
-        pet_image_url = response_data.get("body", {}).get("petImage", {}).get("imageUrl") # 이미지url
         
+        try:
+            # "body"와 "petImage"에서 안전하게 값을 추출
+            pet_image_url = response_data.get("body", {}).get("petImage", {}).get("imageUrl")
+            
+            # imageUrl이 None이면 기본 메시지 출력
+            if pet_image_url is None:
+                print("imageUrl 값이 없습니다.")
+                pet_image_url = "No Image Available"  # 기본값 설정
+            else:
+                print("이미지 URL:", pet_image_url)
+
+        except AttributeError:
+            pet_image_url = "No Image Available"
+
+        print (pet_name, pet_birth, pet_category, pet_weight, pet_image_url)
+
     else:
         print("요청 실패:", response.status_code)
 
-def vaccine_info(pet_id, jwt_token):
+def vaccine_info(petId, jwt_token):
     # 실제 petId 값으로 교체 필요 (프론트에서 받아야함)
     url = f"http://114.70.216.57/pet/api/vaccination/{petId}"
 
