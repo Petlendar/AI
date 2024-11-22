33a4cec5e8a2a8f7500bdc1a4dc81509c14aa144
LeeKyungbum <lkb3311@gmail.com>
2024-11-22 15:11:24 +0900

    #34 (fix): 엔드포인트 변경 및 프롬프트 수정

--- FoodRec.py
+++ FoodRec.py
@@ -10,7 +10,6 @@ def process_food_rec(text, api_key, pet_id, jwt_token):
     messages = [
         {"role": "system", "content": "너는 애완동물 먹이 추천 시스템이야."},
         { "role": "user", "content": text 
-        +"애완 동물 관련 질문이 아니라면\"동물 관련 질문이 아닙니다.\"라는 응답만을 해줘(알겠습니다 앞으로 이렇게 응답하겠습니다라는 말 없이)"
         +f"{pet_name}은(는) 생년 월일이{pet_birth}이고, 종은 {pet_category}, 몸무게는 {pet_weight}야. 이에 맞춤 음식 또는 사료를 추천해줘."
         +"공백 포함 300자 이내로 답변할 것."
         }
--- PetAdvice.py
+++ PetAdvice.py
@@ -11,7 +11,6 @@ def process_pet_advice(text, api_key, pet_id, jwt_token):
     messages = [
         {"role": "system", "content": "너는 반려동물 관련 상담 시스템이야."},
         {"role": "user", "content":
-         "애완 동물 관련 질문이 아니라면\"동물 관련 질문이 아닙니다. 동물 관련 질문을 해주세요.\"라는 응답만을 해줘(알겠습니다 앞으로 이렇게 응답하겠습니다라는 말 없이)"
         +f"애완 동물 이름은 {pet_name}이고, 생년 월일은{pet_birth}이고, 종은 {pet_category}, 몸무게는 {pet_weight}야."
         +"공백 포함 300자 이내로 답변할 것."
         + text}
